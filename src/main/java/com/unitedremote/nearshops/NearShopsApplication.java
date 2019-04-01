f52a8573826d1becd3636463e10b7003469068bb
mac <elhaloui@yandex.com>
2019-04-01 20:20:43 +0100

    init spring boot project

---
+++ src/main/java/com/unitedremote/nearshops/NearShopsApplication.java
@@ -0,0 +1,13 @@
+package com.unitedremote.nearshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearShopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NearShopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unitedremote/nearshops/NearShopsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.unitedremote.nearshops;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NearShopsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
